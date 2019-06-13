7b5af59175c612da9aa92eec46da570c4470b589
Tobias Mautes <tobias.mautes@coodoo.de>
2019-06-13 16:12:25 +0200

    implemented score counter
    added drawing score counter to flutty_game.dart;
    adjusted bird impulse velocity;
    made flutty_world check for beaten obstacles and keep track of score;
    made default level spawn 50 random obstacles;
    removed leftover files;

--- lib/components/flutty_world.dart
+++ lib/components/flutty_world.dart
@@ -1,14 +1,24 @@
+import 'dart:math';
+
 import 'package:flame/box2d/box2d_component.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutty_bird/components/levels/default.dart';
 import 'bird.dart';
 
 class FluttyWorld extends Box2DComponent {
   BirdComponent bird;
+  num score = 0;
+  List<bool> pointGranted = List<bool>();
+  Random random = Random();
 
   FluttyWorld() : super(scale: 4.0);
   List<BodyComponent> _bodies;
+
   void initializeWorld() {
     _bodies = new DefaultLevel(this).bodies;
+    _bodies.forEach((body) {
+      pointGranted.add(false);
+    });
     bird = new BirdComponent(this);
     addAll(_bodies);
     add(bird);
@@ -18,18 +28,37 @@ class FluttyWorld extends Box2DComponent {
   void update(t) {
     super.update(t);
     bird.update(t);
-    // var i = 0;
+
+    var i = 0;
+
+    //print(_bodies[1].body.position.x);
+    //print(bird.body.position.x);
     _bodies.forEach((body) {
-      if(bird.body.position.distanceTo(body.body.position) < 21){
-        if (body is CollisionBody) {
-        remove(bird);
-        bird = new BirdComponent(this);
-        add(bird);
+      if (body.body.position.x < bird.body.position.x) {
+        //don't score walls
+        if (i > 2) {
+          if (pointGranted[i] == false) {
+            score++;
+            pointGranted[i] = true;
+          }
         }
       }
-      // i++;
+
+      if (bird.body.position.distanceTo(body.body.position) < 21) {
+        if (body is CollisionBody) {
+          remove(bird);
+          pointGranted.clear();
+          for (var i = 0; i < _bodies.length; i++) {
+            this.pointGranted.add(false);
+          }
+          score = 0;
+          bird = new BirdComponent(this);
+          add(bird);
+        }
+      }
+      i++;
       body.update(t);
     });
-    cameraFollow(bird, horizontal: 0.4, vertical: 0.4);
+    cameraFollow(bird, horizontal: 0);
   }
 }
--- lib/flutty-game.dart
+++ lib/flutty-game.dart
@@ -1,19 +1,28 @@
-import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'dart:ui';
+import 'dart:ui' as ui;
 import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutty_bird/components/flutty_world.dart';
-import 'package:flutty_bird/util.dart';
 
 class FluttyGame extends Game {
   final FluttyWorld fluttyWorld = new FluttyWorld();
-  // var move = 0;
+  ui.ParagraphBuilder paragraph =
+      new ui.ParagraphBuilder(new ui.ParagraphStyle());
+  String scoreText = 'Score: 0';
+
   FluttyGame() {
+    paragraph.pushStyle(
+        new ui.TextStyle(color: new Color(0xFFFFFFFF), fontSize: 12.0));
     fluttyWorld.initializeWorld();
   }
 
   @override
   void render(Canvas canvas) {
+    paragraph.addText(scoreText);
+    var p = paragraph.build()
+      ..layout(new ui.ParagraphConstraints(width: 180.0));
+    canvas.drawParagraph(p, new Offset(22, 70));
     // Dies war ein Test zum bewegen von boxen
     // var rect = new Rect.fromLTWH(500.0 - move, 350.0, 50.0, 50.0);
     // var paint = new Paint()..color = new Color(0xFFFF0000);
@@ -28,6 +37,9 @@ class FluttyGame extends Game {
   void update(double t) {
     // move++;
     fluttyWorld.update(t);
+
+    num score = fluttyWorld.score;
+    scoreText = 'Score: $score';
   }
 
   @override
